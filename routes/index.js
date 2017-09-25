a2f79f73a30f10dd7a08461f61abd3a17b83fa7d
Artem <artemgergetc@gmail.com>
2017-09-25 18:20:47 +0300

    lp pages tranfered to main site

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,36 @@ router.get('/contacts', function(req, res, next) {
   res.render('pages/contacts');
 });
 
+
+router.get('/lp', function(req, res, next) {
+  res.render('pages/lp/home');
+});
+
+router.get('/sp1/expired', function(req, res, next) {
+  res.render('pages/lp/sp-1-expired');
+});
+router.get('/sp1/thankyou', function(req, res, next) {
+  res.render('pages/lp/sp-1-thankyou');
+});
+router.get('/sp1/thankyou/2', function(req, res, next) {
+  res.render('pages/lp/sp-1-thankyou-2');
+});
+
+router.get('/sp/kiev', function(req, res, next) {
+  res.render('pages/lp/sp-kiev');
+});
+router.get('/sp/kharkiv', function(req, res, next) {
+  res.render('pages/lp/sp-kharkiv');
+});
+router.get('/sp1/kiev', function(req, res, next) {
+  res.render('pages/lp/sp1-kiev');
+});
+router.get('/sp1/kharkiv', function(req, res, next) {
+  res.render('pages/lp/sp1-kharkiv');
+});
+
+router.get('/webinar', function(req, res, next) {
+  res.render('pages/lp/webinar');
+});
+
 module.exports = router;
